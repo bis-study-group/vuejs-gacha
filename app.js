b422aab8441b20b3481803489b6d29abf31fc168
*SLWn7FmdJf <keita.nishimaki@ari-jp.com>
2020-08-06 15:21:00 +0900

    commit1

--- app.js
+++ app.js
@@ -2,15 +2,15 @@ const API_URL = 'https://ruddy-mail.glitch.me/api/gacha';
 
 Vue.component('gif', {
   template: `
-    <figure v-show="/* Insert code here... */">
-      <img :src="/* Insert code here... */" alt="" />
+    <figure v-show="url">
+      <img :src="url" alt="" />
       <figcaption>
         GIFs by <a href="https://giphy.com/">GIPHY</a>
       </figcaption>
     </figure>
   `,
   props: [
-    /* Insert code here... */
+    'url'
   ],
 });
 
@@ -25,7 +25,13 @@ new Vue({
       this.result = '';
 
       // Axiosを使ってAPI呼び出し
-      /* Insert code here... */
+      axios.get(API_URL)
+        .then(response => {
+          this.result = response.data.url;
+        })
+        .catch(error => {
+          this.result = '';
+        })
     },
   },
 });
